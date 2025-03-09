bcf1f6efa49382ba42030dac8ac687366d90a1ca
BalloonsFiesta <balloonsfiesta1508@gmail.com>
2025-03-09 23:02:51 +0530

    solving some errors

--- config/config.php
+++ config/config.php
@@ -1,9 +1,7 @@
 <?php
 ob_start();  // Start output buffering
 session_start();
-// $base_url = 'https://marketplace-foxb.onrender.com/';
-
-$base_url = 'http://localhost:8000/';
+$base_url = 'https://marketplace-foxb.onrender.com/';
 
 date_default_timezone_set('Asia/Kolkata');
 ?>
